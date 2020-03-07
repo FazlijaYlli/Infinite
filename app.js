8eaa398ae2e1d621e0dccf2feff958c91995aa30
Ylli Fazlija <fazlijaylli@gmail.com>
2020-03-07 10:35:44 +0100

    Added some style to the buttons and the "size" tag to fruits

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const name = {
     GREENAPPLE: 'Green Apple',
     PEAR: 'Pear',
     CUCUMBER: 'Cucumber',
-    CARROT: 'Carrot'
+    CARROT: 'Carrot',
+    TOMATO: 'Tomato',
+    CHERRYTOMATO : 'Cherry Tomato',
+    WATERMELON: 'Watermelon',
+    BANANA: 'Banana'
 }
 const color = {
     RED: 'Red',
     ORANGE: 'Orange',
+    YELLOW: 'Yellow',
     GREEN: 'Green',
     EMPTY: 'Empty'
 }
@@ -19,13 +24,24 @@ const shape = {
     EMPTY: 'Empty'
 }
 
+const size = {
+    SMALL: 'Small',
+    NORMAL: 'Normal',
+    BIG: 'Straight',
+    EMPTY: 'Empty'
+}
+
+var cnt = 0;
+
 //Classe Fruit pouvant contenir des attributs venants des structures.
 class Fruit {
-    constructor(name, shape, color) {
+    constructor(name, shape, color, size) {
         console.log("Entered the class Fruit")
         this._name = name;
         this._shape = shape;
         this._color = color;
+        this._size = size;
+        this._goodChoice = false;
     }
 
     get GetName() {
@@ -39,6 +55,10 @@ class Fruit {
     get GetShape() {
         return this._shape;
     }
+
+    get GetShape() {
+        return this._size;
+    }
 }
 
 //Classe qui va stocker les choix de l'utilisateur.
@@ -47,9 +67,12 @@ class Choice {
         console.log("Entered the Choice class")
         this._shape = shape.EMPTY;
         this._color = color.EMPTY;
+        this._size = size.EMPTY;
+        this._previousShape = shape.EMPTY;
+        this._previousColor = shape.EMPTY;
     }
 
-    //Méthode pour mettre à jour le choix de la forme de l'objet recherché.
+    //Méthode pour mettre à josur le choix de la forme de l'objet recherché.
     set SetShape(shape) {
         this._shape = shape;
     }
@@ -58,45 +81,56 @@ class Choice {
         this._color = color;
     }
 
-    get GetColor() {
-        return this._color;
-    }
-
-    get GetShape() {
-        return this._shape;
+    set SetSize(size) {
+        this._size = size;
     }
 }
 
 function Update(element) {
-
     console.log("Entered the function Update")
-
     var correctColor = false;
     var correctShape = false;
+    var correctSize = false;
 
     if (element._shape == choice._shape) {
-        correctColor = true;
-    }
-    if (element._color == choice._color) {
         correctShape = true;
     }
 
+    if (element._color == choice._color) {
+        correctColor = true;
+    }
+
+    if (element._size == choice._size) {
+        correctSize = true;
+    }
+
     if (correctColor == true) {
-        if(correctShape == true) {
-            document.write(element.GetColor + " and " + element.GetShape + " : ")
-            document.write(element.GetName + "<br>")
-        }
+        document.write("La recherche a trouvé : " + element.GetName + "<br>")
     }
 }
 
-//Tableau de mes fruits possèdants différentes propriétés.
-var myFruits = [
-    new Fruit(name.REDAPPLE,shape.ROUND,color.RED),
-    new Fruit(name.GREENAPPLE,shape.ROUND,color.GREEN),
-    new Fruit(name.PEAR,shape.CURVY,color.GREEN),
-    new Fruit(name.CUCUMBER,shape.CURVY,color.GREEN),
-    new Fruit(name.CARROT,shape.STRAIGHT,color.ORANGE)
-];
+function ChoiceColor() {
+    var buttonColorRed = document.createElement("button");
+    buttonColorRed.innerHTML = "RED";
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(buttonColorRed);
+    buttonColorRed.addEventListener("click", function() {
+        choice.SetColor = color.RED; console.log("Bouton rouge appuyé");
+        myFruits.forEach(element => Update(element));
+    });
+
+    var buttonColorGreen = document.createElement("button");
+    buttonColorGreen.innerHTML = "GREEN";
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(buttonColorGreen);
+    buttonColorGreen.addEventListener("click", function() {
+        choice.SetColor = color.GREEN; console.log("Bouton vert appuyé");
+        myFruits.forEach(element => Update(element));
+    });
+}
+
+
+
 
 
 
